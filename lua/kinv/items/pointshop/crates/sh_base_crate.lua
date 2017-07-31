d3cf9fc8837ee5290e69a0d2150524184b211510
Valentin Funk <funk.valentin@gmail.com>
2017-07-31 21:55:23 +0200

    fix: Crate unboxing, key removal and crate removal is now a single, atomic action. Fixes unbox timing exploits.

--- lua/kinv/items/pointshop/crates/sh_base_crate.lua
+++ lua/kinv/items/pointshop/crates/sh_base_crate.lua
@@ -63,11 +63,9 @@ end
 
 function ITEM:OnUse( )
 	local ply = self:GetOwner( )
-	local key = self:GetOwner( ):PS2_GetFirstItemOfClass( self.class:GetRequiredKeyClass( ) )
 
 	return self:Unbox()
 	:Then( function( )
-		Pointshop2Controller:getInstance( ):removeItemFromPlayer( ply, key )
 		KLogf( 4, "Player %s unboxed a crate", ply:Nick( ) )
 	end, function( errid, err )
 		KLogf( 2, "Error unboxing crate item: %s", err )
@@ -77,7 +75,7 @@ end
 function ITEM:GetChanceTable()
 	local sumTbl = {} -- Table with accumulated weights
 	local sum = 0
-	for k, info in ipairs(self.itemMap) do
+	for k, info in ipairs( self.itemMap ) do
 		local factoryClass = getClass( info.factoryClassName )
 		if not factoryClass then
 			KLogf(2, "[WARN] Crate %s invalid factory %s", self.crate:GetPrintName(), info.factoryClassName)
@@ -100,16 +98,48 @@ function ITEM:GetChanceTable()
 				return sum + item
 			end )
 		
-		for _, info in ipairs(chanceMap) do
-			local itemOrInfo, chance = info.itemOrInfo, info.chance
+		for _, chanceInfo in ipairs(chanceMap) do
+			local itemOrInfo, chance = chanceInfo.itemOrInfo, chanceInfo.chance
 			
 			local weight = factoryWeight * ( chance / factoryItemWeightsSum )
+			if not LibK.isProperNumber(weight) then
+				print( factoryWeight, chance, factoryItemWeightsSum,chance / factoryItemWeightsSum, weight )
+				LibK.GLib.Error("BaseCrate:GetChanceTable - Invalid item weight!")
+			end
 			sum = sum + weight
-			table.insert(sumTbl, { sum = sum, itemOrInfo = itemOrInfo, chance = weight })
+			-- Chance represents the actual chance, display
+			table.insert(sumTbl, { sum = sum, itemOrInfo = itemOrInfo, chance = weight, displayChance = factoryWeight })
 		end
 	end
 
 	return sumTbl, sum
+end
+-- Modified from original single use as everything is now wrapped in a transaction
+function ITEM:InternalOnUse( )
+	--Avoid double use
+	if self.used or not self:CanBeUsed( ) then
+		return
+	end
+
+	local succ, err = pcall( self.OnUse, self )
+	if not succ then
+		LibK.GLib.Error( Format( "[ERROR] Item %s Use failed: %s", self.class.name, err ) )
+		return
+	end
+	self.used = true
+
+	local ply = self:GetOwner( )
+	Promise.Resolve()
+	:Then(function() 
+		-- Pcall returns either the argument or the returned value
+		-- By wrapping the return value in a promise here we make sure
+		-- that if a promise is returned we wait on it.
+		return err
+	end):Then( function( )
+		KLogf( 4, "Player %s used an item", ply:Nick( ) )
+	end, function( errid, err )
+		LibK.GLib.Error( Format( "Error using item: %s %s", tostring(errid), tostring(err) ) )
+	end )
 end
 
 function ITEM:PickRandomItems(iterations)
@@ -122,6 +152,7 @@ function ITEM:PickRandomItems(iterations)
 			if info.sum >= r then
 				itemOrInfo = info.itemOrInfo
 				itemOrInfo._chance = info.chance / sum
+				itemOrInfo._displayChance = info.displayChance
 				break
 			end
 		end
@@ -131,29 +162,33 @@ function ITEM:PickRandomItems(iterations)
 
 	local itemsPicked = {}
 	for i = 1, iterations do
-		table.insert(itemsPicked, getRandomItem())
+		local item = getRandomItem()
+		table.insert(itemsPicked, item)
 	end
 	return itemsPicked
 end
 
 function ITEM:Unbox( )
 	local ply = self:GetOwner( )
-
+	local key = self:GetOwner( ):PS2_GetFirstItemOfClass( self.class:GetRequiredKeyClass( ) )
+	if not key then LibK.GLib.Error("ITEM:Unbox - Invalid Key") end
+	
 	local seed = math.random(10000)
 	math.randomseed(seed)
-	local items = self:PickRandomItems(Pointshop2.Drops.WINNING_INDEX )
+	local items = self:PickRandomItems( Pointshop2.Drops.WINNING_INDEX )
 	local winningItem = items[Pointshop2.Drops.WINNING_INDEX]
-	local rarity = Pointshop2.GetRarityInfoFromNormalized(winningItem._chance)
-
+	local rarity = Pointshop2.GetRarityInfoFromAbsolute(winningItem._displayChance)
+	local crateId = self.id
+	local keyId
 	return Promise.Resolve()
 	:Then(function()
 		if winningItem.isInfoTable then
-			return winningItem.createItem()
+			return winningItem.createItem( true )
 		else
 			return winningItem:new( )
 		end
 	end)
-	:Then( function( item )
+	:Then(function(item)
 		local price = item.class:GetBuyPrice( ply )
 		item.purchaseData = {
 			time = os.time( ),
@@ -169,26 +204,60 @@ function ITEM:Unbox( )
 			item.purchaseData.amount = 0
 			item.purchaseData.currency = "points"
 		end
-		return item:save( )
+		
+		item.inventory_id = ply.PS2_Inventory.id
+		item:preSave()
+		keyId = key.id
+		if Pointshop2.DB.CONNECTED_TO_MYSQL then
+			local transaction = LibK.TransactionMysql:new(Pointshop2.DB)
+			transaction:begin()
+			transaction:add(item:getSaveSql()) -- Create Item
+			transaction:add(Format("DELETE FROM kinv_items WHERE id IN(%i, %i)", self.id, key.id)) -- Remove crate & key
+			return transaction:commit():Then(function()
+				return Pointshop2.DB.DoQuery("SELECT LAST_INSERT_ID() as id")
+			end ):Then(function(id)
+				item.id = id[1].id
+				return item
+			end):Then(Promise.Resolve, function(err) 
+				LibK.GLib.Error("BaseCrate:Unbox - Error running sql " + tostring(err))
+				return Pointshop2.DB.DoQuery("ROLLBACK"):Then( function()
+					return Promise.Reject( "Error!" )
+				end )
+			end )
+		else
+			sql.Begin()
+			-- Remove crate and key
+			self:remove(true):Then(function() 
+				return key:remove(true)
+			end):Then(function()
+				item.inventory_id = ply.PS2_Inventory.id
+				return item:save()
+			end):Then(function()
+				sql.Commit()
+			end, function(err)
+				sql.Query("ROLLBACK")
+				return Promise.Reject(err)
+			end)
+			return Promise.Resolve(item)
+		end
 	end )
 	:Then( function( item )
-		KInventory.ITEMS[item.id] = item
-		return ply.PS2_Inventory:addItem( item )
-		:Then( function( )
-			KLogf( 4, "Player %s unboxed %s, got item %s", ply:Nick( ), self:GetPrintName( ) or self.class.PrintName, item:GetPrintName( ) or item.class.PrintName )
-			item:OnPurchased( )
-			return item
-		end )
-		:Then( function(item) 
-			Pointshop2.Drops.DisplayCrateOpenDialog({
-				ply = ply,
-				crateItemId = self.id,
-				seed = seed,
-				wonItemId = item.id
-			})
+		Pointshop2.Drops.DisplayCrateOpenDialog({
+			ply = ply,
+			crateItemId = crateId,
+			seed = seed,
+			wonItemId = item.id
+		})
 
-			return Promise.Delay(10, item) -- Wait for animation before broadcasting the chat message
-		end )
+		KInventory.ITEMS[crateId] = nil
+		KInventory.ITEMS[keyId] = nil
+		ply.PS2_Inventory:notifyItemRemoved(crateId)
+		ply.PS2_Inventory:notifyItemRemoved(keyId)
+		ply.PS2_Inventory:notifyItemAdded(item)
+
+		KLogf( 4, "Player %s unboxed %s, got item %s", ply:Nick( ), self:GetPrintName( ) or self.class.PrintName, item:GetPrintName( ) or item.class.PrintName )
+		item:OnPurchased( )
+		return Promise.Delay(10, item) -- Wait for animation before broadcasting the chat message
 	end )
 	:Fail( function( errid, err )
 		KLogf( 2, "[ERROR UNBOX] Error: %s %s", tostring( errid ), tostring( err ) )
